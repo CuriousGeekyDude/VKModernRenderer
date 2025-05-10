492ce81c6397fae622d9fe82b20ad83c26ce309a
Farhan Aghnoum <130616138+CuriousGeekyDude@users.noreply.github.com>
2025-05-10 20:56:02 +0430

    Update bat file

--- InitializeRequiredFolders.bat
+++ InitializeRequiredFolders.bat
@@ -1,7 +1,7 @@
 @echo off
 :: Create 'Assets' directory in C:\Users\farhan\source\repos\VKModernRenderer
 mkdir "%~dp0Assets"
-mkdire "%~dp0Assets\Sponza"
+mkdir "%~dp0Assets\Sponza"
 
 :: Create 'Binary Scene Files' directory inside 'InitFiles' directory in current directory
 mkdir "%~dp0InitFiles\Binary Scene Files"
